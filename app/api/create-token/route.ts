1b6a7601fa53cacd69577c1e3b5f976cf0e6ddbd
igdev <dorultanianos@gmail.com>
2024-10-15 20:21:35 +0300

    refactor: file upload rotue

--- app/api/create-token/route.ts
+++ app/api/create-token/route.ts
@@ -1,4 +1,4 @@
-import PinataClient from '@pinata/sdk';
+import PinataClient, {PinataPinResponse} from '@pinata/sdk';
 import path from 'path';
 import {writeFile} from 'fs/promises';
 import {NextApiResponse} from 'next';
@@ -18,6 +18,27 @@ interface TokenMetadata {
   icon: string | null,
   name: string,
 }
+async function processFileUploadToIPFS(binaries: ArrayBuffer, file_name: string): Promise<string> {
+  // Convert the file data to a Buffer
+    const buffer = Buffer.from(await binaries);
+    const filename = file_name.replaceAll(" ", "_");
+
+    try {
+      // Write the file to the specified directory (public/assets) with the modified filename
+      const filepath = path.join(process.cwd(), "/static/" + filename);
+      await writeFile(
+          filepath,
+          buffer
+      );
+
+      const fileStream = fs.createReadStream(filepath);
+      // Upload the file to Pinata
+      const {IpfsHash} = await sdk.pinFileToIPFS(fileStream, {pinataMetadata: {name: filename}});
+      return `${FILE_UPLOAD_BASE_URL}${IpfsHash}`
+    } catch (err) {
+      throw err;
+    }
+}
 
 export async function POST(req: NextRequest, res: NextApiResponse) {
   // Parse the incoming form data
@@ -27,6 +48,7 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
   if(!token_name) {
     return NextResponse.json({message: "Invalid input, token_name is required", }, {status: 400});
   }
+
 
   const tokenMetadata: TokenMetadata = {
     name: formData?.get("token_name") as string,
@@ -39,37 +61,15 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
     // If no file is received, return a JSON response with an error and a 400 status code
     return res.json({ error: "No files received."});
   }
-
-  // Convert the file data to a Buffer
-  if ('arrayBuffer' in file) {
-    const buffer = Buffer.from(await file.arrayBuffer());
-    const filename = file.name.replaceAll(" ", "_");
-
+  if ("arrayBuffer" in file) {
     try {
-      // Write the file to the specified directory (public/assets) with the modified filename
-      const filepath = path.join(process.cwd(), "/static/" + filename);
-      await writeFile(
-          filepath,
-          buffer
-      );
-
-      const fileStream = fs.createReadStream(filepath);
-
-
-      // Upload the file to Pinata
-      const result = await sdk.pinFileToIPFS(fileStream, {pinataMetadata: {name: filename}});
-      tokenMetadata.icon = `${FILE_UPLOAD_BASE_URL}${result.IpfsHash}`;
-      // const fileUrl = await sdk.get
-      // Return a JSON response with a success message and a 201 status code
-      return NextResponse.json({ message: "success", data: result }, {status: 201});
+      const icon_uri = await processFileUploadToIPFS(await file.arrayBuffer(), file.name);
+      tokenMetadata.icon = icon_uri;
 
     } catch (err) {
-      // If an error occurs during file writing, log the error and return a JSON response with a failure message and a 500 status code
-      return NextResponse.json({ message: "Server error, failed to upload to pinada"}, {status: 500});
+        return NextResponse.json({message: "Failed while trying to process file upload", details: err.message}, {status: 500})
     }
+
   }
-
-
-
-  return NextResponse.json({message: "Success"});
+    return NextResponse.json({message: "Success"});
 }
