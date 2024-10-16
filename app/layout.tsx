78a0b0d2e8fa4ddfa4ca719a0fc28e86f806ae24
igdev <dorultanianos@gmail.com>
2024-10-16 15:15:32 +0300

    refactor: layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Theme appearance="dark" accentColor="purple">
+      <Theme appearance="dark" accentColor="purple" scaling="110%">
         {children}
       </Theme>
       </body>
